4174e168078c8deac8a6e6d48a06274570532bfc
martinolee <soohan.m.lee@gmail.com>
2020-07-12 21:16:34 +0900

    Add UIViewUtility.swift

---
+++ Source/UIViewUtility.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewUtility.swift
+//  Pods-UIKitUtility_Example
+//
+//  Created by Soohan Lee on 2020/07/12.
+//
+
+import UIKit
+
+protocol UIViewUtility {
+  /**
+  Adds views to the end of the receiver’s list of subviews.
+  
+  - parameter views: The views to be added. After being added, these views appear on top of any other subviews.
+  */
+  func addSubviews(_ views: [UIView])
+}
+
+extension UIView: UIViewUtility {
+  func addSubviews(_ views: [UIView]) {
+    for view in views {
+      self.addSubview(view)
+    }
+  }
+}
